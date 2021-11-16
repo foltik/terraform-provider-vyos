1812b479c03824a734970dd984da5306a92a3f52
Thomas Finstad Larsen <thomas.finstad.larsen@gmail.com>
2021-11-16 18:37:09 +0100

    Initial generic helpers and config block helpers
    These helpers are to make config block (not tree or single value) type
    resources very easy to add.
    
    When saying config block what is meant is several config keys under the
    same path, any sub path will be ignored. This is usefull when you must
    configre more than one parameter at the same time to have a valid vyos
    commit, or if you do not wish for any sub paths to be part of the
    resource.
    
    See "firewall rule set" as an example where we do not wish to
    affect the sub paths for all the firewall rules in the set.

---
+++ vyos/resource_firewall_rule_set.go
@@ -0,0 +1,82 @@
+package vyos
+
+import (
+	"context"
+	"regexp"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	"github.com/foltik/vyos-client-go/client"
+)
+
+const (
+	ResourceFirewallRuleSetKeyTemplate = "firewall name {{name}}"
+)
+
+func resourceRuleSet() *schema.Resource {
+	return &schema.Resource{
+		Description:   "A rule-set is a named collection of firewall rules that can be applied to an interface or a zone, for more information see [VyOS Firewall doc](https://docs.vyos.io/en/latest/configuration/firewall/index.html#overview).",
+		CreateContext: resourceRuleSetCreate,
+		ReadContext:   resourceRuleSetRead,
+		UpdateContext: resourceRuleSetUpdate,
+		DeleteContext: resourceRuleSetDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Description: "The resource ID, same as the `name`",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"name": {
+				Description:      "Name for this rule-set, _must be unique_.",
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringMatch(regexp.MustCompile("^[-A-Za-z]+$"), "Rule-set name can only contain letters and hyphens.")),
+				ForceNew:         true,
+			},
+			"description": {
+				Description: "Rule-set description text.",
+				Type:        schema.TypeString,
+				Default:     "Manged by Terraform.",
+				Optional:    true,
+			},
+			"default_action": {
+				Description:      "Default action of the rule-set if no rule matched a packet criteria.",
+				Type:             schema.TypeString,
+				Default:          "drop",
+				Optional:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringInSlice([]string{"accept", "reject", "drop"}, false)),
+			},
+			"enable_default_log": {
+				Description: "Enable the logging of the default action.",
+				Type:        schema.TypeBool,
+				Default:     false,
+				Optional:    true,
+			},
+		},
+	}
+}
+
+func resourceRuleSetRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*client.Client)
+	return helper_config_block_read(ctx, client, ResourceFirewallRuleSetKeyTemplate, d, resourceRuleSet().Schema)
+}
+
+func resourceRuleSetCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*client.Client)
+	return helper_config_block_create(ctx, client, ResourceFirewallRuleSetKeyTemplate, d, resourceRuleSet().Schema)
+}
+
+func resourceRuleSetUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*client.Client)
+	return helper_config_block_update(ctx, client, ResourceFirewallRuleSetKeyTemplate, d, resourceRuleSet().Schema)
+}
+
+func resourceRuleSetDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*client.Client)
+	return helper_config_block_delete(ctx, client, ResourceFirewallRuleSetKeyTemplate, d, resourceRuleSet().Schema)
+}
