6e89683fe64ed1712e908faf90cd8b7f7051de8f
angelnu <git@angelnucom>
2022-06-06 14:21:25 +0000

    support list values as json encoded strings

--- examples/resources/vyos_config_block_tree/resource.tf
+++ examples/resources/vyos_config_block_tree/resource.tf
@@ -8,3 +8,13 @@ resource "vyos_config_block" "allow_sith_supremacy" {
     "destination group port-group": "Jedi"
   }
 }
+
+resource "vyos_config_block_tree" "ssh" {
+  path = "service ssh"
+
+  configs = {
+    "port"      = "22",
+    "disable-password-authentication" = "", #Keep simple passwords for login via terminal but require key for ssh
+    "listen-address" = jsonencode(["192.168.2.1", "192.168.63.1"]) # Listen in LAN and management interface
+  }
+}
